afb18b720f4256f4497af141a4cb7328eeb2763a
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-04-18 21:04:04 -0700

    adding mongoose models and controllers

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var controllers = require('../controllers');
 
 router.get('/:resource', function(req, res, next) {
   var resource = req.params.resource
 
-  res.json({
-    confirmation: 'success',
-    resource: resource
-  })
+  var controller = controllers[resource]
+  // error block for if user request unrecognized api
+  if (controller == null){
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid Resouce'
+    })
+    return
+  }
 
+  // success block
+  controller.get(req.query, false)
+  .then(function(results){
+    res.json({
+      confirmation: 'success',
+      results: results
+    })
+  })
+  .catch(function(err){
+    res.json({
+      confirmation: 'fail',
+      message: err
+    })
+  })
 });
 
 module.exports = router;
