98fcd7756e02dd4572551fda223096d0bbbfb625
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-06-14 23:50:49 -0700

    updating files and debugging

--- src/app.js
+++ src/app.js
@@ -6,11 +6,11 @@ class App extends Component {
     render(){
         return (
             <div>
-                Hello World. Backend is up and running 
+                Hello World. Backend is up and running
                 <Posts />
             </div>
         )
     }
 }
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
